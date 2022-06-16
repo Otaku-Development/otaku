411af53a48d76afb17012c70ac5476f91eedb05f
Liam Richardson <hello@discoliam.com>
2022-06-16 17:09:36 +0100

    Git init

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const Image = require('@11ty/eleventy-img')
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [null],
+    formats: ['webp', 'jpeg', 'svg'],
+    outputDir: 'dist/assets/images',
+    urlPath: '/assets/images',
+    svgShortCircuit: true,
+  })
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async',
+  }
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes)
+}
+
+module.exports = (config) => {
+  config.addPlugin(eleventyNavigationPlugin)
+  config.addNunjucksAsyncShortcode('image', imageShortcode)
+  config.addJavaScriptFunction('image', imageShortcode)
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      data: 'data',
+      output: 'dist',
+    },
+  }
+}
