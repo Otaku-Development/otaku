48689aa30e8b0cf08ed4025f48f1dca86f8354dc
Liam Richardson <hello@discoliam.com>
2022-07-07 16:59:17 +0100

    Maybe adding a form?

---
+++ src/assets/js/form.js
@@ -0,0 +1,47 @@
+// Ajax Form
+
+// Vars
+const form = document.getElementById('ContactForm')
+const contactMessageSuccess = document.querySelector(
+  '.Contact__MessageText--success'
+)
+const contactMessageError = document.querySelector(
+  '.Contact__MessageText--error'
+)
+
+export default function () {
+  if (form) {
+    form.addEventListener('submit', handleSubmit)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let formData = new FormData(form)
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(displaySuccessMessage())
+      .catch(displayerrorMessage(error))
+  }
+
+  const displaySuccessMessage = () => {
+    contactMessageSuccess.classList.add('Contact__MessageText--active')
+    const successTimeout = setTimeout(hideSuccessMessage, 5000)
+    function hideSuccessMessage() {
+      contactMessageSuccess.classList.remove('Contact__MessageText--active')
+      clearTimeout(successTimeout)
+    }
+  }
+
+  const displayerrorMessage = (error) => {
+    console.log(error)
+    contactMessageSuccess.classList.add('Contact__MessageText--active')
+    const errorTimeout = setTimeout(hideerrorMessage, 5000)
+    function hideerrorMessage() {
+      contactMessageError.classList.remove('Contact__MessageText--active')
+      clearTimeout(errorTimeout)
+    }
+  }
+}
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import Highway from '@dogstudio/highway'
 import Fade from './transition.fade.js'
 import menuInit from './menu.js'
+import form from './form.js'
 
 const H = new Highway.Core({
   transitions: {
@@ -9,3 +10,4 @@ const H = new Highway.Core({
 })
 
 menuInit()
+form()
